39cccb89f747435c01cc08b3eca87009cd979b58
Tsion <tsionzewdu461@gmail.com>
2025-11-07 12:38:46 -0800

    Add simple_interest.py for simple interest calculator exercise

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+principal = 1000      
+rate = 0.05          
+time = 3              
+
+# Calculate simple interest
+interest = principal * rate * time
+
+
+print("The simple interest is:", interest)
+
